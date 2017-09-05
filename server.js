801c7d554683e34bd03b18fb36e33c681f72404b
dineshbs97 <31442555+dineshbs97@users.noreply.github.com>
2017-09-05 11:30:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/article-two',function(req,res){
     res.send("please wait ...siva working to serve you");
 });
 app.get('/manoj',function(req,res){
-    res.send("<pr> Dinesh under processing </pr>");
+    res.send(" Dinesh under processing ");
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
