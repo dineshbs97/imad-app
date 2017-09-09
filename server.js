ad6c38dcb3cb2101bf4769d7f90af5830a21039f
dineshbs97 <31442555+dineshbs97@users.noreply.github.com>
2017-09-08 21:44:14 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,9 +76,9 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function(req,res){
     //articleName== article-one
-    //articles[articleName]== {} content object for article one
+    //articles[articleName]== {} content object for article-one
     var articleName= req.params.articleName;
-    res.send(createTemplate(articles[aricleName]));
+    res.send(createTemplate(articles(aricleName)));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
