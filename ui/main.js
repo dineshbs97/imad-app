d31d53958eac4e48a08da0120d7cc442c7809e98
dineshbs97 <31442555+dineshbs97@users.noreply.github.com>
2017-09-10 02:33:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML= "Hi Dinesh" ;
 
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+ marginLeft = marginLeft + 5;
+ img.style.marginLeft=  marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = serInterval(moveRight, 50);
 };
