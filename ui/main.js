83e5bd512b5e7b081e122c373860f7df1555372a
dineshbs97 <31442555+dineshbs97@users.noreply.github.com>
2017-09-10 02:10:22 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element =document.getElementById('main-text');
 element.innerHTML= "Hi Dinesh" ;
 
 // Move the image
-var img = document.getElementById("madi");
+var img = document.getElementById('madi');
 img.onclick=function () {
  img.style.marginLeft="100px";
 };
