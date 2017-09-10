012467ff387e548d4c24e098e7c65140d4a718bc
dineshbs97 <31442555+dineshbs97@users.noreply.github.com>
2017-09-10 06:07:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@
       //Create a request object
       var request = new XMLHttpRequest();
       
-      // capture the request and store in a variable
+      //Capture the request and store in a variable
       request.onsteadystatechange = function () {
                   if(request.readystate===XMLHttpRequest.Done){
                             //Take some action
@@ -22,5 +22,4 @@
       //Make the Request
       request.open('GET','http://dineshbs97.imad.hasura-app.io', true);
       request.send( null );
-    
   };
